0d8ba8bac13069f825b54df0a0e5d6e9153b066f
Robert Nelson <robertcnelson@gmail.com>
2024-06-17 15:28:43 -0500

    Merge TI Branch; https://github.com/RobertCNelson/ti-linux-kernel/compare/76313798e7121bf56820e7aa7aed82a0f3ffc566...51479baa13b77d89091fef60bac4a5707870b15a TI SDK: 10.00.02 BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/fd4444acc89822275d3facf18b49f796915b80db WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=1156a08e910614d9ea4e197a75c9aeb369ce0aa3 WPANUSB: https://openbeagle.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -510,14 +510,14 @@ drivers () {
 #	dir 'mikrobus'
 	dir 'drivers/android'
 
-#	#cd KERNEL/
-#	#git checkout v5.10-rc1 -b tmp
-#	#git pull --no-edit https://git.kernel.org/pub/scm/linux/kernel/git/geert/renesas-drivers.git topic/overlays-v5.10-rc1
-#	#mkdir ../patches/overlays
-#	#git format-patch -12 -o ../patches/overlays/
-#	#https://git.kernel.org/pub/scm/linux/kernel/git/geert/renesas-drivers.git/log/?h=topic/overlays-v5.10-rc1
-#	#../
-#	#dir 'overlays'
+	#cd KERNEL/
+	#git checkout v5.10-rc1 -b tmp
+	#git pull --no-edit https://git.kernel.org/pub/scm/linux/kernel/git/geert/renesas-drivers.git topic/overlays-v5.10-rc1
+	#mkdir ../patches/overlays
+	#git format-patch -12 -o ../patches/overlays/
+	#https://git.kernel.org/pub/scm/linux/kernel/git/geert/renesas-drivers.git/log/?h=topic/overlays-v5.10-rc1
+	#../
+	#dir 'overlays'
 
 #	dir 'drivers/ar1021_i2c'
 
